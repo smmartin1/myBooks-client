dcec0e7656473224d6c9c950a3eea2d9dea595c7
smmartin1 <smarie333m@gmail.com>
2023-01-31 10:43:38 -0500

    Added Cards and Links

--- src/components/book-card/book-card.jsx
+++ src/components/book-card/book-card.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
-export function BookCard({ book, onBookClick }) {
+export function BookCard({ book }) {
     return (
-        <div onClick={() => { onBookClick(book)}}>{book.Title}</div>
+        <Card className="book-card">
+            <Card.Img crossOrigin="anonymous" variant="top" src={book.ImagePath} />
+            <Card.Body className="book-body">
+                <Card.Title className="card-title">{book.Title}</Card.Title>
+                <Link to={`/books/${book._id}`}>
+                    <Button id="book-btn" variant="primary">Open</Button>
+                </Link>
+                
+            </Card.Body>
+        </Card>
     );
 }
 
 BookCard.propTypes = {
     book: PropTypes.shape({
         Title: PropTypes.string.isRequired
-    }).isRequired,
-    onBookClick: PropTypes.func.isRequired
+    }).isRequired
 };
